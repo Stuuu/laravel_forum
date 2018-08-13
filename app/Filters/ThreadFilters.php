aac117732726964f25e44b0b6898a38133f8c4f4
Stuuu <stuartschrick@gmail.com>
2018-08-12 22:26:29 -0400

    Refactor filters class

--- app/Filters/ThreadFilters.php
+++ app/Filters/ThreadFilters.php
@@ -5,24 +5,20 @@ namespace App\Filters;
 use App\User;
 use Illuminate\Http\Request;
 
-class ThreadFilters
+class ThreadFilters extends Filters
 {
-    protected $request;
-
-
-    public function __construct(Request $request)
+    protected $filters = ['by'];
+    
+    /**
+     * Filter the query by a given username.
+     *
+     * @param $string $username
+     * $return mixed
+     */
+    public function by($username)
     {
-        $this->request = $request;
-    }
-
-    public function apply($builder)
-    {
-        if (! $username = $this->request->by) {
-            return $builder;
-        }
-        
         $user = User::where('name', $username)->firstOrFail();
         
-        return $builder->where('user_id', $user->id);
+        return $this->builder->where('user_id', $user->id);
     }
 }
