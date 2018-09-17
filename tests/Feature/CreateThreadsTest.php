a4f1b369dfce85026a7767780d787ec1f1e8672c
Stuuu <stuartschrick@gmail.com>
2018-09-16 20:49:10 -0400

    Put in empty test with TODO for more defined auth check for deletion

--- tests/Feature/CreateThreadsTest.php
+++ tests/Feature/CreateThreadsTest.php
@@ -93,6 +93,12 @@ class CreateThreadsTest extends TestCase
         $this->assertDatabaseMissing('replies', ['id' => $reply->id]);
     }
 
+    /** @test */
+    public function threads_may_only_be_deleted_by_those_who_have_permission()
+    {
+        //TODO:
+    }
+
     public function publishThread($overrides = [ ])
     {
         $this->withExceptionHandling()->signIn();
